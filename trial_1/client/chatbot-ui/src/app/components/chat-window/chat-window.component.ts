6ae425b0459da2868ca8aebbb3b5be6674d5a4e6
Ajay <ajay@Ajays-MacBook-Pro.local>
2025-09-28 12:34:22 +0530

    Toaster and badhe added

--- trial_1/client/chatbot-ui/src/app/components/chat-window/chat-window.component.ts
+++ trial_1/client/chatbot-ui/src/app/components/chat-window/chat-window.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
-import { MatTabsModule } from '@angular/material/tabs';
+import { Observable, Subscription } from 'rxjs';
+import { skip } from 'rxjs/operators';
+import { MatTabsModule, MatTabChangeEvent } from '@angular/material/tabs';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { MessageListComponent } from '../message-list/message-list.component';
 import { MessageFormComponent } from '../message-form/message-form.component';
@@ -16,6 +19,8 @@ import { WebsocketService } from '../../services/websocket.service';
   imports: [
     CommonModule,
     MatTabsModule,
+    MatBadgeModule,
+    MatSnackBarModule,
     MessageListComponent,
     MessageFormComponent,
     CourseChipsComponent,
@@ -24,21 +29,48 @@ import { WebsocketService } from '../../services/websocket.service';
   templateUrl: './chat-window.component.html',
   styleUrls: ['./chat-window.component.css'],
 })
-export class ChatWindowComponent {
+export class ChatWindowComponent implements OnInit, OnDestroy {
   coursesForChips$: Observable<string[] | null>;
   courseInfoData$: Observable<any[] | null>;
+  showCourseInfoBadge = false;
+  private courseInfoSub!: Subscription;
 
   constructor(
     private messageService: MessageService,
-    private websocketService: WebsocketService
+    private websocketService: WebsocketService,
+    private snackBar: MatSnackBar,
+    private cdr: ChangeDetectorRef
   ) {
     this.coursesForChips$ = this.messageService.courseChips$;
     this.courseInfoData$ = this.messageService.courseInfo$;
+  }
+
+  ngOnInit(): void {
+    this.courseInfoSub = this.messageService.courseInfo$.pipe(skip(1)).subscribe(data => {
+      if (data && data.length > 0) {
+        this.showCourseInfoBadge = true;
+        this.snackBar.open(`${data.length} courses found. Click on the Course Info tab for details.`, 'Dismiss', {
+        });
+        this.cdr.markForCheck();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.courseInfoSub) {
+      this.courseInfoSub.unsubscribe();
+    }
   }
 
   onCourseSelected(course: string): void {
     const newMessage = `I would like to pursue ${course}`;
     this.messageService.addMessage(newMessage, 'user');
     this.websocketService.sendMessage({ text: newMessage });
+  }
+
+  onTabChange(event: MatTabChangeEvent): void {
+    if (event.index === 1) {
+      this.showCourseInfoBadge = false;
+    }
   }
 }
