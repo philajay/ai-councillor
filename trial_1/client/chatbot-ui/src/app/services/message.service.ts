d4d9e285e544fa84dd12d3bfc0fd7e432cba31af
Ajay <ajay@Ajays-MacBook-Pro.local>
2025-10-15 13:04:42 +0530

    Added formatting to json message

--- trial_1/client/chatbot-ui/src/app/services/message.service.ts
+++ trial_1/client/chatbot-ui/src/app/services/message.service.ts
@@ -197,19 +197,17 @@ export class MessageService {
     };
 
     if(agentName === 'extract_order_entity' || agentName === 'auto_agent'){
-      console.log(JSON.stringify(jsonData))
+      console.log(JSON.stringify(jsonData,  null, 2))
     }
 
     if (agentName === 'extract_order_entity' && jsonData.clarification_question) {
       console.log(`Clarification question found ${JSON.stringify(jsonData)}`);
       message.text = jsonData.clarification_question;
-      console.log(jsonData["reason"])
     } else if (jsonData.agentId) {
       message.text = jsonData.purpose;
     }  else if(lastMessage.agent === "auto_agent"){
         message.isJson = false;
         message.text = jsonData["markdown"]
-        console.log(jsonData["reason"])
     } else {
       //message.text = "Processing request. Hang tight.."
       return
